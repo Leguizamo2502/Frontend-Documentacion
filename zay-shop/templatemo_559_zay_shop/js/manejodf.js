f2d5d76b5a26cc90db4767e9cf9f634913744cb6
Maria Isabel Tovar Pastrana <isabeltovarp.18@gmail.com>
2024-10-26 15:13:01 -0500

    documentos

--- zay-shop/templatemo_559_zay_shop/js/manejodf.js
+++ zay-shop/templatemo_559_zay_shop/js/manejodf.js
@@ -22,3 +22,59 @@ function mostrarResumen() {
     });
     document.getElementById("resumen").classList.remove('oculto');
 }
+
+function mostrarDescripcion(){
+    const iframes = document.querySelectorAll('.iframes');
+    iframes.forEach(iframe => {
+        iframe.classList.add('oculto');
+    });
+    document.getElementById("descripcion").classList.remove('oculto');
+}
+
+function mostrarSolucion(){
+    const iframes = document.querySelectorAll('.iframes');
+    iframes.forEach(iframe => {
+        iframe.classList.add('oculto');
+    });
+    document.getElementById("solucion").classList.remove('oculto');
+}
+
+function mostrarPlanTrabajo(){
+    const iframes = document.querySelectorAll('.iframes');
+    iframes.forEach(iframe => {
+        iframe.classList.add('oculto');
+    });
+    document.getElementById("planTrabajo").classList.remove('oculto');
+}
+
+function mostrarEquipoTrabajo(){
+    const iframes = document.querySelectorAll('.iframes');
+    iframes.forEach(iframe => {
+        iframe.classList.add('oculto');
+    });
+    document.getElementById("equipoTrabajo").classList.remove('oculto');
+}
+
+function mostrarPresupuesto(){
+    const iframes = document.querySelectorAll('.iframes');
+    iframes.forEach(iframe => {
+        iframe.classList.add('oculto');
+    });
+    document.getElementById("presupuesto").classList.remove('oculto');
+}
+
+function mostrarPlanMantenimiento(){
+    const iframes = document.querySelectorAll('.iframes');
+    iframes.forEach(iframe => {
+        iframe.classList.add('oculto');
+    });
+    document.getElementById("mantenimiento").classList.remove('oculto');
+}
+
+function mostrarCronogramaPagos(){
+    const iframes = document.querySelectorAll('.iframes');
+    iframes.forEach(iframe => {
+        iframe.classList.add('oculto');
+    });
+    document.getElementById("cronogramaPagos").classList.remove('oculto');
+}
